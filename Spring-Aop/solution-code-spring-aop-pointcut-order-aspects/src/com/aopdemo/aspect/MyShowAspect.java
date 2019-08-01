ab5e46e528a87d0fcc5730c4485fbaf4c8f01e4a
prasadgtuvce <prasadgt@ymail.com>
2019-08-02 01:53:29 +0530

    Add files via upload

---
+++ Spring-Aop/solution-code-spring-aop-pointcut-order-aspects/src/com/aopdemo/aspect/MyShowAspect.java
@@ -0,0 +1,18 @@
+package com.aopdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Order(6)
+public class MyShowAspect {
+
+	@Before("com.aopdemo.aspect.AopExpressions.forDaoPackageNoGetterSetter()")
+	public void performApiAnalytics() {
+		System.out.println("\n=====>>> SHOW");		
+	}
+
+}
